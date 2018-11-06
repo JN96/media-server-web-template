c5b5344fbe27314c084ae9be760b519c502c1629
James <james@DESKTOP-VVTSASE.localdomain>
2018-11-06 20:19:33 +0000

    read in file, display file, play file and video player js and css

---
+++ public/javascript/javascript.js
@@ -0,0 +1,86 @@
+window.onload = function() {
+	const videoList = document.querySelector('.video-list');
+	const player = document.querySelector('.player');
+	const video = player.querySelector('.viewer');
+	const progress = player.querySelector('.progress');
+	const progressBar = player.querySelector('.progress__filled');
+	const toggle = player.querySelector('.toggle');
+	const skipButtons = player.querySelectorAll('[data-skip]');
+	const ranges = player.querySelectorAll('.player__slider');
+
+	fetch('/data')
+	.then(
+		function(response) {
+			if (response.status !== 200) {
+				console.log('Looks like there was a problem. Status Code: ' +
+					response.status);
+				return;
+			}
+
+			response.json().then(function(dataSrc) {
+				console.log(dataSrc);
+				dataSrc.forEach(src => {
+					console.log(src);
+					var p = document.createElement('p');
+					p.innerHTML = `${src}`;
+					videoList.appendChild(p);
+					video.src = src;
+				});
+			});
+		}
+		)
+	.catch(function(err) {
+		console.log('Fetch Error :-S', err);
+	});
+
+// ========================================================
+// 					Video Player Controls
+// ========================================================
+function togglePlay() {
+	const method = video.paused ? 'play' : 'pause';
+	video[method]();
+}
+
+function updateButton() {
+	const icon = this.paused ? '►' : '❚ ❚';
+	console.log(icon);
+	toggle.textContent = icon;
+}
+
+function skip() {
+	video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+	video[this.name] = this.value;
+}
+
+function handleProgress() {
+	const percent = (video.currentTime / video.duration) * 100;
+	progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+	const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+	video.currentTime = scrubTime;
+}
+
+/* Hook up the event listeners */
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+} // end document ready
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
+
+var video = path.join('video.mp4');
+// var video = path.join(__dirname, '../public', 'video.mp4');
+// var videoString = fs.readFileSync(video, 'utf8');
+var videoPathArray = [];
+videoPathArray.push(video);
+console.log(video);
+console.log('video path array: ', videoPathArray);
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
+});
+
+router.get('/data', function(req, res, next){
+	res.send(videoPathArray);
 });
 
 module.exports = router;
