5f35e62f92f0655ba623b7cd21e3f748169f8d67
Mark Coyle <markcoyle14@hotmail.com>
2018-11-11 12:14:40 +0000

    Find nested files

--- actions/FileLocator.js
+++ actions/FileLocator.js
@@ -2,6 +2,6 @@ const glob = require('glob');
 const path = require('path');
 module.exports = {
     fetchVideos: function (src, callback) {
-        glob(src + '/**/*.mp4', callback);
+        glob(src + '/**/*.+(mp4|mkv)', callback);
     }
 };
